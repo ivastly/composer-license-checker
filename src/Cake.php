ee87784f28be4f288db49d74763170bf9acb56df
vasily.pyatykh <vasily.pyatykh@lendable.co.uk>
2025-07-21 17:52:57 +0200

    ready for review

---
+++ src/Cake.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Lendable\ComposerLicenseChecker;
+
+
+final class Cake
+{
+    public function __construct(
+        private int $candles,
+    ) {
+    }
+
+    public function removeCandle(): void
+    {
+        $this->candles--;
+    }
+
+    public function __toString(): string
+    {
+        return "The cake with {$this->candles} candles on the top.\n";
+    }
+}
+
+$cake = new Cake(candles: 10);
+$cake->removeCandle();
+
+echo $cake;
